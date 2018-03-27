e39a814dbe272117ef610fcdfd37ba26fce78dc4
jatindhorajiya <jatin.dhorajiya@gmail.com>
2018-03-27 12:19:21 +0530

    ticket pdf generate

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 	$connect_error = 'Sorry, there was some connectivity issue!';
-	@mysql_connect('localhost','root','') or die($connect_error);
+	@mysql_connect('localhost:3308','root','') or die($connect_error);
 	@mysql_select_db('airline') or die($connect_error);
 
 ?>
